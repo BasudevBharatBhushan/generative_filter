9b0e47d0873f1d0cf388fc58a6e13c07b1505b20
BasudevBharatBhushan <bharatbhushan173@gmail.com>
2023-05-13 03:03:21 +0530

    generate-image api made dynamic

--- api/api.py
+++ api/api.py
@@ -5,7 +5,10 @@ from typing import List
 from bson import ObjectId
 from api.methods import get_collection
 from scripts.generate_image import blend_image
+from typing import Optional
+from utils.logging import logging
 import base64
+import json
 
 app = APIRouter()
 
@@ -62,28 +65,92 @@ async def get_image(image_id: str, category: str = None):
         raise HTTPException(status_code=500, detail="Internal server error")
 
 @app.post("/generate-image")
-async def generate_image(background_id: str, body_id: str, clothes_id: str, goggles_id: str, head_id: str):
+async def generate_image(ids: dict):
     try:
+        background_id = ids.get("background_id")
+        body_id = ids.get("body_id")
+        cloths_id = ids.get("cloths_id")
+        goggles_id = ids.get("goggles_id")
+        head_id = ids.get("head_id")
 
-        existing_image = generated_images_collection.find_one({"blended_images.01Backgrounds.id": background_id, "blended_images.02Body.id": body_id, "blended_images.03Cloths.id": clothes_id, "blended_images.04Goggles.id": goggles_id, "blended_images.05Head.id": head_id})
+        existing_image = generated_images_collection.find_one({
+            "blended_images.01Backgrounds.id": background_id,
+            "blended_images.02Body.id": body_id,
+            "blended_images.03Cloths.id": cloths_id,
+            "blended_images.04Goggles.id": goggles_id,
+            "blended_images.05Head.id": head_id
+        })
+
         if existing_image is not None:
-            return {"id": str(existing_image.get("_id")), "name":"generated_image" ,"blended_images": existing_image.get("blended_images") ,"data": str(base64.b64encode(existing_image.get("data")), 'utf-8')}
-        
-        # Blend the images
+            logging.info("Image already exists in the database")
+            return {
+                "id": str(existing_image.get("_id")),
+                "name": existing_image.get("name"),
+                "blended_images": existing_image.get("blended_images"),
+                "data": existing_image.get("data")
+            }
 
-        blended_image = await blend_image(background_id, body_id, clothes_id, goggles_id, head_id)
+        # Blend the images
+        blended_image = await blend_image(background_id, body_id, cloths_id, goggles_id, head_id)
 
         blended_image_pattern = blended_image.get("blended_images")
         blended_image_data = blended_image.get("data")
-        
-        # Store the blended image in the database
-        result = generated_images_collection.insert_one({"name": "generated_image","blended_images": blended_image_pattern,"data":blended_image_data})
 
-        # Return the blended image id
-        return {"id": str(result.inserted_id), "name":"generated_image" ,"blended_images": blended_image_pattern ,"data": str(base64.b64encode(blended_image_data), 'utf-8')}
-       
-    except:
+        return {
+            "name": "generated_image",
+            "blended_images": blended_image_pattern,
+            "data": str(base64.b64encode(blended_image_data), 'utf-8')
+        }
+
+    except Exception as e:
+        logging.error(f"Failed to generate image: {str(e)}")
         raise HTTPException(status_code=500, detail="Internal server error")
+
+@app.post("/save-data")
+async def save_data(data: dict):
+    try:
+        blended_images = data.get("blended_images")
+        if blended_images is None:
+            raise HTTPException(status_code=400, detail="Invalid request: blended_images field is missing")
+
+        # Check if the blended images already exist in the database
+        existing_image = generated_images_collection.find_one({
+            "blended_images.01Backgrounds.id": blended_images.get("01Backgrounds", {}).get("id"),
+            "blended_images.02Body.id": blended_images.get("02Body", {}).get("id"),
+            "blended_images.03Cloths.id": blended_images.get("03Cloths", {}).get("id"),
+            "blended_images.04Goggles.id": blended_images.get("04Goggles", {}).get("id"),
+            "blended_images.05Head.id": blended_images.get("05Head", {}).get("id")
+        })
+
+        if existing_image is not None:
+            logging.info("Image already exists in the database")
+            return {
+                "id": str(existing_image.get("_id")),
+                "name": existing_image.get("name"),
+                "blended_images": existing_image.get("blended_images"),
+                "data": data.get("data")
+            }
+
+        # Save the data in the database
+        result = generated_images_collection.insert_one(data)
+
+        # Retrieve the inserted document ID
+        document_id = str(result.inserted_id)
+
+        # Add the ID to the JSON data
+        data["_id"] = document_id
+
+        return {
+            "id": data.get("_id"),
+            "name": data.get("name"),
+            "blended_images": data.get("blended_images"),
+            "data": data.get("data")
+        }
+
+    except Exception as e:
+        logging.error(f"Failed to save data: {str(e)}")
+        raise HTTPException(status_code=500, detail="Internal server error")
+
 
 
 @app.get("/generated-image/all")
--- scripts/generate_image.py
+++ scripts/generate_image.py
@@ -3,62 +3,62 @@ from PIL import Image
 import io
 import base64
 from api import api
+from typing import Optional
 
 
-async def blend_image(background_id: str, body_id: str, clothes_id: str, goggles_id: str, head_id: str):
+async def blend_image(background_id: Optional[str] = None, body_id: Optional[str] = None, clothes_id: Optional[str] = None, goggles_id: Optional[str] = None, head_id: Optional[str] = None):
     try:
-        # Fetch the images from the database
-        background_data = await api.get_image(background_id, "01Backgrounds")
-        body_data = await api.get_image(body_id, "02Body")
-        clothes_data = await api.get_image(clothes_id, "03Cloths")
-        goggles_data = await api.get_image(goggles_id, "04Goggles")
-        head_data = await api.get_image(head_id, "05Head")
+        # Fetch the images from the database based on the provided IDs
+        images = {
+            "01Backgrounds": background_id,
+            "02Body": body_id,
+            "03Cloths": clothes_id,
+            "04Goggles": goggles_id,
+            "05Head": head_id
+        }
 
-        # Check if any image is missing
-        if background_data is None or body_data is None or clothes_data is None or goggles_data is None or head_data is None:
-            raise HTTPException(status_code=404, detail="One or more images not found")
+        loaded_images = {}
+        loaded_images_data = {}
+        for category, image_id in images.items():
+            if image_id is not None:
+                image_data = await api.get_image(image_id, category)
+                if image_data is None:
+                    raise HTTPException(status_code=404, detail=f"Image not found for category {category}")
+                loaded_images[category] = {
+                    "id": image_id,
+                    "name": image_data.get("name"),
+                }
+                loaded_images_data[category]={
+                    "data":image_data.get("data"),  # base64 encoded image data
+                }
 
-        # Load the images
-        img1 = Image.open(io.BytesIO(base64.b64decode(background_data.get('data')))).convert("RGBA")
-        img2 = Image.open(io.BytesIO(base64.b64decode(body_data.get('data')))).convert('RGBA')
-        img3 = Image.open(io.BytesIO(base64.b64decode(clothes_data.get('data')))).convert('RGBA')
-        img4 = Image.open(io.BytesIO(base64.b64decode(goggles_data.get('data')))).convert('RGBA')
-        img5 = Image.open(io.BytesIO(base64.b64decode(head_data.get('data')))).convert('RGBA')
 
-        # Blend the images together
-        blended_img = Image.alpha_composite(Image.alpha_composite(Image.alpha_composite(Image.alpha_composite(img1, img2), img3), img4), img5)
+        # Check if any images are loaded
+        if len(loaded_images) == 0:
+            raise HTTPException(status_code=400, detail="No valid image IDs provided")
 
-        # Save the blended image to a new file
+        # # Load the images and blend them together
+        blended_img = None
+        for category, image_data in loaded_images_data.items():
+            img = Image.open(io.BytesIO(base64.b64decode(image_data.get('data')))).convert('RGBA')
+            if blended_img is None:
+                blended_img = img
+            else:
+                blended_img = Image.alpha_composite(blended_img, img)
+
+        # # Save the blended image to a new file
         with io.BytesIO() as buffer:
             blended_img.save(buffer, format='PNG')
             data = buffer.getvalue()
 
-        # Return the blended data along with the image names
+
+        blended_img.save("new.png", format="png")
+        # # Return the blended data along with the loaded image names
+
         return {
-            "blended_images": {
-                "01Backgrounds": {
-                    "id": background_id,
-                    "name": background_data.get("name")
-                },
-                "02Body": {
-                    "id": body_id,
-                    "name": body_data.get("name")
-                },
-                "03Cloths": {
-                    "id": clothes_id,
-                    "name": clothes_data.get("name")
-                },
-                "04Goggles": {
-                    "id": goggles_id,
-                    "name": goggles_data.get("name")
-                },
-                "05Head": {
-                    "id": head_id,
-                    "name": head_data.get("name")
-                }
-            },
+            "blended_images": loaded_images,
             "data": data
         }
+
     except:
         raise HTTPException(status_code=500, detail="Internal server error")
-
